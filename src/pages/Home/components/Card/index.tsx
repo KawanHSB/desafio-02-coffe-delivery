c17adabafc8a1ee8509843afc868d7d6dd1f7c71
KawanHSB <kawanbatista@hotmail.com>
2023-03-15 19:14:04 -0300

    coffeCard style done

--- src/pages/Home/components/Card/index.tsx
+++ src/pages/Home/components/Card/index.tsx
@@ -1,18 +1,32 @@
 import styles from './styles.module.scss'
 import ExpressoTradicional from '../../../../assets/expresso-tradicional.png'
+import { FaShoppingCart } from 'react-icons/fa'
 
 export default function Card() {
   return (
     <div className={styles.coffeCard}>
       <img src={ExpressoTradicional} alt="foto do expresso tradicional" />
-      <span>
+      <span className={styles.tag}>
         <p>TRADICIONAL</p>
       </span>
       <h3>EXPRESSO TRADICIONAL</h3>
       <p className={styles.label}>
         O tradicional café feito com água quente e grãos moídos
       </p>
-      <div className={styles.buyWraper}></div>
+      <div className={styles.buyWraper}>
+        <span className={styles.value}>
+          <p>R$</p>
+          <h3>9,90</h3>
+        </span>
+        <span className={styles.itens}>
+          <button>-</button>
+          <p>1</p>
+          <button>+</button>
+        </span>
+        <span className={styles.cartIconContainer}>
+          <FaShoppingCart />
+        </span>
+      </div>
     </div>
   )
 }
