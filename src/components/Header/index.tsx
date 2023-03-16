3e7c354753c47bf6038fa7f69e37593cf44e9c19
KawanHSB <kawanbatista@hotmail.com>
2023-03-16 18:03:19 -0300

    header style correction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,17 +5,19 @@ import Logo from '../../assets/Logo.svg'
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <img src={Logo} alt="" />
-      <div className={styles.locateCart}>
-        <span className={styles.locate}>
-          <IoLocationSharp className={styles.locateIcon} />
-          <p>porto alegre</p>
-        </span>
-        <span className={styles.cart}>
-          <FaShoppingCart className={styles.cartIcon} />
-        </span>
-      </div>
-    </header>
+    <div className={styles.headerWraper}>
+      <header className={styles.header}>
+        <img src={Logo} alt="" />
+        <div className={styles.locateCart}>
+          <span className={styles.locate}>
+            <IoLocationSharp className={styles.locateIcon} />
+            <p>porto alegre</p>
+          </span>
+          <span className={styles.cart}>
+            <FaShoppingCart className={styles.cartIcon} />
+          </span>
+        </div>
+      </header>
+    </div>
   )
 }
