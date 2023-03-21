7b9cc7a8c13de2dd56c0053348e6fed5585463e3
KawanHSB <kawanbatista@hotmail.com>
2023-03-21 17:59:32 -0300

    Form Add

--- src/pages/Checkout/components/CompleteSeuPedido/index.tsx
+++ src/pages/Checkout/components/CompleteSeuPedido/index.tsx
@@ -1,8 +1,35 @@
-import styles from './style.module.scss'
+import styles from './styles.module.scss'
+import { MapPinLine } from '@phosphor-icons/react'
 export function CompleteSeuPedido() {
   return (
-    <>
+    <div className={styles.componentWraper}>
       <h3>Complete Seu Pedido</h3>
-    </>
+      <div className={styles.endereco}>
+        <span>
+          <span className={styles.title}>
+            <MapPinLine className={styles.locateIcon} />
+            <span>
+              <h5>Endereço de Entrega</h5>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </span>
+          </span>
+          <form className={styles.formAdress}></form>
+        </span>
+      </div>
+      <div className={styles.pagamento}></div>
+    </div>
   )
 }
+
+// <div className={styles.endereco}>
+// <span className={styles.title}>
+//   <span>
+//     <span>
+//       <MapPinLine className={styles.locateIcon} />
+//       <h5>Endereço de Entrega</h5>
+//       <p>Informe o endereço onde deseja receber seu pedido</p>
+//     </span>
+//     <form className={styles.formAdress}></form>
+//   </span>
+// </span>
+// </div>
