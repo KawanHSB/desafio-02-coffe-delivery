301bc73705db1587d73472db5152eb54d74a9f8c
KawanHSB <kawanbatista@hotmail.com>
2023-03-27 18:58:08 -0300

    checkout form created

--- src/pages/Checkout/components/CafesSelecionados/index.tsx
+++ src/pages/Checkout/components/CafesSelecionados/index.tsx
@@ -1,8 +1,8 @@
-// import styles from './style.module.scss'
+import styles from './style.module.scss'
 export function CafesSelecionados() {
   return (
-    <>
+    <div className={styles.componentWraper}>
       <h3>Cafés Selecionados</h3>
-    </>
+    </div>
   )
 }
--- src/pages/Checkout/components/CompleteSeuPedido/index.tsx
+++ src/pages/Checkout/components/CompleteSeuPedido/index.tsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss'
-import { MapPinLine } from '@phosphor-icons/react'
+import { MapPinLine, CurrencyDollar } from '@phosphor-icons/react'
 export function CompleteSeuPedido() {
   return (
     <div className={styles.componentWraper}>
@@ -13,10 +13,30 @@ export function CompleteSeuPedido() {
               <p>Informe o endereço onde deseja receber seu pedido</p>
             </span>
           </span>
-          <form className={styles.formAdress}></form>
+          <form className={styles.formAdress}>
+            <input className={styles.cep} placeholder="cep" />
+            <input className={styles.rua} placeholder="rua" />
+            <input className={styles.numero} placeholder="numero" />
+            <input className={styles.complemento} placeholder="complemento" />
+            <input className={styles.cidade} placeholder="cidade" />
+            <input className={styles.uf} placeholder="uf" />
+            <input className={styles.ufSigla} placeholder="ufSigla" />
+          </form>
         </span>
       </div>
-      <div className={styles.pagamento}></div>
+      <div className={styles.pagamento}>
+        <span className={styles.title}>
+          <span>
+            <CurrencyDollar className={styles.currencyICon} />
+          </span>
+          <span>
+            <h5>Pagamento</h5>
+            <p>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </p>
+          </span>
+        </span>
+      </div>
     </div>
   )
 }
